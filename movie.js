adebd7e4e94b86cf4c32a6e706f9c74995105c4a
MaruKate <mashakasha3001@gmail.com>
2021-05-31 11:57:48 +0300

    changed seat and row

--- movie.js
+++ movie.js
@@ -1,31 +1,25 @@
 let address = document.location;
-console.log(address.search);
 
 let URL = address.search.slice(1);
-console.log(URL);
 
 let arrURL = URL.split(';');
-console.log(arrURL);
 
 let movieData = { };
 
 for (let k = 0; k < arrURL.length; k += 1) {
 	let curr = arrURL[k].split('=');
-	console.log(curr);
 	movieData[curr[0]] = curr[1];
 }
 
-console.log(movieData);
 
 let contain = document.getElementById("cont");
 contain.innerHTML = `
 	<div id="wrapper">
 	<div id="blur">
-	<div id="content"><p class="title"></p>
+	<div id="content">
 	<h1 class="title" id="movieName">Тут будет название!</h1>
-	<p class="title" id="row"></p>
-	<p class="title" id="seat"></p>
-	<p class="title">Приятного просмотра</p></div>
+	<div id="place"><p id="row"></p></div>
+	<p class="title" id="text">Приятного просмотра</p></div>
 	<iframe id="moviePlayer" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div></div>
 	`
@@ -35,10 +29,8 @@ let wrapper = document.getElementById("wrapper");
 let movieLink = document.getElementById("moviePlayer");
 let movieName = document.getElementById("movieName");
 let row = document.getElementById("row");
-let seat = document.getElementById("seat");
 
 wrapper.style.backgroundImage = `url(${movieData.movieImg})`;
 movieLink.src = movieData.movieLink;
 movieName.innerHTML = decodeURI(movieData.movieName);
-row.innerHTML = `Ваш ряд - ${movieData.movieRow}`;
-seat.innerHTML = `Ваше место - ${movieData.movieSeat}`;
+row.innerHTML = `Ваш ряд - ${movieData.movieRow}, место - ${movieData.movieSeat}`;
